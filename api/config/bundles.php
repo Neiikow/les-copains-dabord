877714203df84c222c99e36cff1362f3830e8be2
Neiikow <bergerjordan24@gmail.com>
2019-06-18 00:22:38 +0200

    API: Ajout du bundle Security et sa configuration

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -10,4 +10,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
